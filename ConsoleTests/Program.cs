68dee48692e935e572776e8120c4f28afcf0b4f1
yzk <yzk365@qq.com>
2021-02-02 10:00:18 +0800

    降低对组件的版本要求

--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -23,7 +23,7 @@ namespace ConsoleTests
 
             using (var sp = services.BuildServiceProvider())
             {
-                /*
+                
                 var authApi = sp.GetRequiredService<AuthApi>();
                 var getAccessTokenResult = await authApi.GetAccessTokenAsync(appId, appSecret);
                 string accessToken = getAccessTokenResult.Access_token;
@@ -37,8 +37,7 @@ namespace ConsoleTests
                 else
                 {
                     Console.WriteLine(r.Errmsg);
-                }*/
-                var authApi = sp.GetRequiredService<AuthApi>();
+                }
                 //code只能用一次
                 string code = "091YGwGa1h6BsA0K23Ga1KrZUJ1YGwGI";
                 string encryptedData = "tBfph8cMa2z/CvXVrxemSzBKkmeAQSJD0zveHylwPqV58Ir1mQiXdPYYaAPSK4zkwIJbu5GHWJKGRqLA8B7xx1ThwX53l7w82T4DDMK3mAipAGax1oxb8CoHAzU0ZuXgT4ejdNnf+hty1QKWY4X3UBoLt0Ies32AGYk1YelfRVxStVJ2Tf2BGHuHli3k4t1eH3Ra/3y9i8d9+kCF3sypwZY/T0mSR9GMqlWLVR0/NrH0vGHqqBRL48dikcgZA1+4Qy9R+Z/Anz4BCj2xbBQ1euCIRA7SAcB8x9oswxwh3DbY/46sYo7BcooLAv0lPOUQjKFlW4osA0qv4rtKW4qTAVebmgZcYNRAnco9BGWlqFCSPobB9rrX2M5d6AQLcFg2bmuceWqXCXY0LIfAr6NZpYcLggrCcHfUtVt+Lv9R9cWtTwYgjNE3ywjMhNUxN8Gv2JTxEe98V/Rbu590ZuTvOuxUfVU91Ko18+Bc3PcILd8=";
